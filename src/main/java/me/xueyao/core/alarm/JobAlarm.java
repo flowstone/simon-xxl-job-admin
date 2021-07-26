63ace2cc0580a7c72f8491d0d0c0496228dfd258
simonxue <xueyao.me@gmail.com>
2021-07-26 22:00:44 +0800

    feat:初始化

---
+++ src/main/java/me/xueyao/core/alarm/JobAlarm.java
@@ -0,0 +1,21 @@
+package me.xueyao.core.alarm;
+
+
+import me.xueyao.core.model.XxlJobInfo;
+import me.xueyao.core.model.XxlJobLog;
+
+/**
+ * @author xuxueli 2020-01-19
+ */
+public interface JobAlarm {
+
+    /**
+     * job alarm
+     *
+     * @param info
+     * @param jobLog
+     * @return
+     */
+    public boolean doAlarm(XxlJobInfo info, XxlJobLog jobLog);
+
+}
